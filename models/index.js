88606cd4e1e41cd6cc51bd87ca7aad1096a6cca8
Victor Montelongo <victoramontelongo@gmail.com>
2023-11-15 09:16:39 -0600

    added the models index belongs to associations.

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasmany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    Model: ProductTag,
+    foreignKey: 'product_id'
+  }
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  }
+})
 
 module.exports = {
   Product,
