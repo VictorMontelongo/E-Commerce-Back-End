a6c8b9fd45c94dddaf76c794c9fc8ffbbd681613
Victor Montelongo <victoramontelongo@gmail.com>
2023-11-15 09:32:45 -0600

    fixed index in models

--- models/index.js
+++ models/index.js
@@ -5,22 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-Category.hasmany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    Model: ProductTag,
-    foreignKey: 'product_id'
-  }
+  through: ProductTag,
+  as: 'productTag_products',
+  foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    foreignKey: 'tag_id'
-  }
+  through: ProductTag,
+  as: 'productTag_products',
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
